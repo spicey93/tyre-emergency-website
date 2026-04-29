0ac74fab2a97a80c5f186764bc532f91b2a30ae7
spicey93 <spicerdean@icloud.com>
2026-04-29 06:17:06 +0100

    more key components created

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Use this site to convert favicon.svg to .ico file: https://cloudconvert.com/svg-
 
 Fake logos on: https://logoipsum.com/
 
+Manual entry points:
+- Assets i.e. logo.png
+- Main.scss i.e. primary colour
+- src/consts.ts i.e. name, phone
+- favicon
--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,24 @@
-export const NAME = "Company name";
-export const DESCRIPTION = "Company description";
-export const PHONE = "01234567890";
-export const EMAIL = "email@email.com";
-export const ADDRESS = "1 Example Road";
-export const CITY = "City";
-export const COUNTY = "Countyshire";
-export const POSTCODE = "EX14 PLE";
-export const VAT_NO = "GB123456789";
-export const COMPANY_NO = "12345678";
-export const FACEBOOK = "tyreemergencyltd"
-export const INSTAGRAM = "tyreemergencyltd"
-export const YOUTUBE = "@TyreEmergencyLtd"
-export const GMB = "p3hK2J8INUjzdzcZL"
+export const NAME = "Tyre Emergency Ltd";
+export const DESCRIPTION =
+  "Stuck with a flat tyre? We'll come to you FAST - typically in 60 minutes or less. Based in Nottinghamshire. We operate 24 hours a day, 7 days a week.";
+export const PHONE = "0330 133 9311";
+export const EMAIL = "contact@tyreemergency.com";
+export const ADDRESS = "Bilsthorpe";
+export const CITY = "Newark";
+export const COUNTY = "Nottinghamshire";
+export const POSTCODE = "NG22 8GA";
+export const VAT_NO = "GB456597051";
+export const COMPANY_NO = "15362025";
+export const FACEBOOK = "tyreemergencyltd";
+export const INSTAGRAM = "tyreemergencyltd";
+export const YOUTUBE = "@TyreEmergencyLtd";
+export const TWITTER = "tyre_emergency";
+export const GMB = "UglmQxsSPUYKmNpQM";
+export const TIKTOK = "@tyreemergencyltd";
+export const YEAR = "2026";
+export const SERVICES = [
+  { link: "mobile-tyre-fitting", title: "Mobile Tyre Fitting", content: "" },
+  { link: "locking-wheel-nut-removal", title: "Locking Wheel Nut Removal", content: "" },
+];
+export const GOOGLE_RATING = "5.0"
+export const GOOGLE_REVIEWS = "458"
