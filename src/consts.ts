c7b0f8068bc0c1b83a184f55c05f2e80330c4d4f
spicey93 <spicerdean@icloud.com>
2026-04-30 05:54:53 +0100

    more content added

--- src/consts.ts
+++ src/consts.ts
@@ -17,8 +17,63 @@ export const GMB = "UglmQxsSPUYKmNpQM";
 export const TIKTOK = "@tyreemergencyltd";
 export const YEAR = "2026";
 export const SERVICES = [
-  { link: "mobile-tyre-fitting", title: "Mobile Tyre Fitting", content: "" },
-  { link: "locking-wheel-nut-removal", title: "Locking Wheel Nut Removal", content: "" },
+  {
+    link: "emergecy-tyre-replacement",
+    title: "Emergency Tyre Replacement",
+    cardContent:
+      "Stranded with a flat tyre and need help urgently? We've got you. With an average ETA of 30 minutes, we are one of the fastest mobile tyre services in the UK.",
+  },
+  {
+    link: "mobile-tyre-fitting",
+    title: "Mobile Tyre Fitting",
+    cardContent:
+      "Need new tyres for your car but don't want to drive to a garage? We can fit them at a location and time that suits you.",
+  },
+  {
+    link: "mobile-caravan-tyre-fitting",
+    title: "Mobile Caravan Tyre Fitting",
+    cardContent:
+      "Why tow your caravan to a tyre shop when you can have them changed on-site instead! Save time and hassle with our mobile service.",
+  },
+  {
+    link: "mobile-tyre-repair",
+    title: "Mobile Tyre Repair",
+    cardContent:
+      "Slow puncture? Nail in your tyre? Get it fixed at home or work with our mobile puncture repair service.",
+  },
+  {
+    link: "locking-wheel-nut-removal",
+    title: "Locking Wheel Nut Removal",
+    cardContent:
+      "Lost your locking wheel nut key? No worries. We've got all the tools to remove them at your location.",
+  },
+  {
+    link: "tpms-sensor-replacement",
+    title: "TPMS Sensor Replacement",
+    cardContent:
+      "Faulty tyre pressure sensor? We can replace it today! Mobile fitting and programming available.",
+  },
 ];
-export const GOOGLE_RATING = "5.0"
-export const GOOGLE_REVIEWS = "458"
+export const GOOGLE_RATING = "5.0";
+export const GOOGLE_REVIEWS = "458";
+export const PAGES = {
+  index: {
+    title: "Home",
+    description: "",
+    heroHeading: "Flat Tyre? Stranded Roadside?",
+    heroSubheading: "Our emergency tyre fitters can come to you ASAP and get you back on the road. Get in touch with us today.",
+  },
+  services: {
+    title: "Services",
+    description:
+      "While we are known for our speedy emergency service, we do offer additional services such as locking wheel nut removal, caravan tyre replacement, and TPMS sensors.",
+    content: "While we are known for our speedy emergency service, we do offer additional services such as locking wheel nut removal, caravan tyre replacement, and TPMS sensors.",
+  },
+  areas: {
+    title: "Areas",
+    description: "",
+    content: "",
+  },
+  about: { title: "About", description: "", content: "" },
+  contact: { title: "Contact", description: "", content: "" },
+};
